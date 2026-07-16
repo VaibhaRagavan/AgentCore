67f6d46cc888ce4147ccdbd28c2a54b8ce2cffb3
Vaibha Ragavan <vaibha97@gmail.com>
2026-07-16 10:41:26 +0100

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -14,7 +14,10 @@ if "server_pinged" not in st.session_state:
     threading.Thread(target=wake_server, daemon=True).start()
     st.session_state["server_pinged"] = True
 st.set_page_config(page_title="AgentCore", page_icon="🤖",layout="wide")
-
+#––––waking up the mcp server–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+with st.spinner("Waking up the assistant... first request may take up to a minute"):
+    result = asyncio.run(main(query,history))
+    st.markdown(result)
 WELCOME="Hello 👋 I can help you with Weather, Wikipedia, and News.\n\n💡 For weather, include the country for accuracy e.g. *Dublin, Ireland* or *Mumbai, India*"
 
 #––––session state–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
